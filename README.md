a3eb70dc74ec49a9eea68c2f08806d0d2d23b320
Gabe Dalay <gabedalay@gmail.com>
2019-01-27 09:30:27 -0500

    Fix some logic, add standard deviation using running stats

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ts-dsp-learn
-
+### This is a work in progress!
 Typescipt implementations of equaitions presented in Science and Engineer's Guide to Digital Signal Processing 
 
 https://www.analog.com/en/education/education-library/scientist_engineers_guide.html
+
--- src/chapterTwo.ts
+++ src/chapterTwo.ts
@@ -2,7 +2,7 @@
 function mean(x:[number]): number {
     let n = x.length;
     let sum = 0;
-    for (let i = 0; i < (n - 1); i++){
+    for (let i = 0; i < n; i++){
         sum += x[i];
     }
     return sum / n;
@@ -10,10 +10,21 @@ function mean(x:[number]): number {
 
 // 2-2
 function standardDeviation(mean:number, x:[number]): number {
-    let n = x.length - 1;
+    let n = x.length;
     let sum = 0;
     for(let i = 0; i < n; i++){
         sum += (x[i] - mean) ** 2
     }
-    return Math.sqrt((sum/n))
+    return Math.sqrt(sum/(n - 1))
+}
+
+
+// 2-3
+function standardDeviationRunningStats(x:[number]): number {
+    let n = x.length, sumOfSquares = 0, sum = 0;
+    for (let i = 0; i < n; i++) {
+        sumOfSquares += x[i] ** 2;
+        sum += x[i];
+    }
+    return (sumOfSquares - ((sum ** 2)/n))/ (n-1);
 }
