5eefb925c56cc38f3d547aea15905fee3e7cf8b1
Gabe Dalay <gabedalay@gmail.com>
2019-01-28 21:06:08 -0500

    Add a few more functions

--- src/chapterTwo.ts
+++ src/chapterTwo.ts
@@ -1,30 +1,44 @@
 // 2-1
-function mean(x:[number]): number {
+function mean(x: [number]): number {
     let n = x.length;
     let sum = 0;
-    for (let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         sum += x[i];
     }
     return sum / n;
 }
 
 // 2-2
-function standardDeviation(mean:number, x:[number]): number {
+function standardDeviation(mean: number, x: [number]): number {
     let n = x.length;
     let sum = 0;
-    for(let i = 0; i < n; i++){
-        sum += (x[i] - mean) ** 2
+    for (let i = 0; i < n; i++) {
+        sum += (x[i] - mean) ** 2;
     }
-    return Math.sqrt(sum/(n - 1))
+    return Math.sqrt(sum / (n - 1));
 }
 
-
 // 2-3
-function standardDeviationRunningStats(x:[number]): number {
-    let n = x.length, sumOfSquares = 0, sum = 0;
+function standardDeviationRunningStats(x: [number]): number {
+    let n = x.length;
+    let sumOfSquares = 0;
+    let sum = 0;
     for (let i = 0; i < n; i++) {
         sumOfSquares += x[i] ** 2;
         sum += x[i];
     }
-    return (sumOfSquares - ((sum ** 2)/n))/ (n-1);
+    return (sumOfSquares - sum ** 2 / n) / (n - 1);
+}
+
+// 2-3a
+function signalNoiseRatio(x: [number]): number {
+    let m = mean(x);
+    let sd = standardDeviation(m, x);
+    return m / sd;
+}
+
+function coefficientOfVariationx(x: [number]): number {
+    let m = mean(x);
+    let sd = standardDeviation(m, x);
+    return sd / m;
 }
